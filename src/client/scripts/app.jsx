0fffb9bd00d3f79e378594a6cd86b2968cf73fb1
Jon Hall <jon_hall@outlook.com>
2016-04-19 22:21:35 +0100

    cards

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,16 +1,50 @@
 import React from 'react'
+import classNames from 'classnames'
 import TitleCard from './title-card.jsx'
+import TitleBar from './title-bar.jsx'
+import Card from './card.jsx'
 /* eslint-disable no-unused-vars */
 import __style from '../styles/app.styl'
 /* eslint-enable no-unused-vars */
 
+// FIXME: Why indent broken when using babel parser?
+/* eslint-disable react/jsx-indent */
 export default class App extends React.Component {
     constructor() {
         super()
         this.state = {
             focusTitle: true,
             collapsed: false,
-            hoverActive: false
+            active: false,
+            projects: [{
+                title: 'Lorem Ipsum',
+                image: '#',
+                description: 'Lorems the ipsum'
+            }, {
+                title: 'Lorem Ipsum',
+                image: '#',
+                description: 'Lorems the ipsum'
+            }, {
+                title: 'Lorem Ipsum',
+                image: '#',
+                description: 'Lorems the ipsum'
+            }, {
+                title: 'Lorem Ipsum',
+                image: '#',
+                description: 'Lorems the ipsum'
+            }, {
+                title: 'Lorem Ipsum',
+                image: '#',
+                description: 'Lorems the ipsum'
+            }, {
+                title: 'Lorem Ipsum',
+                image: '#',
+                description: 'Lorems the ipsum'
+            }, {
+                title: 'Lorem Ipsum',
+                image: '#',
+                description: 'Lorems the ipsum'
+            }]
         }
     }
 
@@ -24,15 +58,29 @@ export default class App extends React.Component {
         setTimeout(() => this.setState({ focusTitle: false }), baseDelay)
 
         // Start the in-place state +2s after the others
-        setTimeout(() => this.setState({ hoverActive: true }), baseDelay + 2000)
+        setTimeout(() => this.setState({ active: true }), baseDelay + 2000)
     }
 
     render() {
-        return (<TitleCard
-            focused={this.state.focusTitle}
-            collapsed={this.state.collapsed}
-            hoverActive={this.state.hoverActive}
-            collapsibles={['jon', 'hall']}
-            tld='me' />)
+        const cards = this.state.projects.map((project) => (<Card {...project} />))
+
+        return (<div className={classNames({
+            app: true,
+            active: this.state.active,
+            'focus-title': this.state.focusTitle
+        })}>
+            <TitleBar>
+                <TitleCard
+                    focused={this.state.focusTitle}
+                    collapsed={this.state.collapsed}
+                    hoverActive={this.state.active}
+                    collapsibles={['jon', 'hall']}
+                    tld='me' />
+            </TitleBar>
+            <div className='card-rack'>
+                {cards}
+            </div>
+        </div>)
     }
 }
+/* eslint-enable react/jsx-indent */
