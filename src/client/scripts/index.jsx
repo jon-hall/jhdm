8f00e5a42b1499a38dfddfe35180ac1346e435c2
Jon Hall <jon_hall@outlook.com>
2016-06-04 20:36:00 +0100

    remove scripts

--- src/client/scripts/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Menu from './menu.jsx'
-// import Pager from './pager.jsx'
-/* eslint-disable no-unused-vars */
-import __style from '../styles/index.styl'
-/* eslint-enable no-unused-vars */
-
-ReactDOM.render(<div><Menu /><div></div></div>, document.querySelector('#app'))
