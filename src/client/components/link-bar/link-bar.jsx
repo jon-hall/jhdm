739274d9bcbbcd416bdadc8733b1fe30edd39a26
Jon Hall <jon_hall@outlook.com>
2016-06-12 19:39:02 +0100

    link bar placeholder

--- src/client/components/link-bar/link-bar.jsx
+++ src/client/components/link-bar/link-bar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 // import classNames from 'classnames'
 /* eslint-disable no-unused-vars */
-import __style from './projects.styl'
+import __style from './link-bar.styl'
 /* eslint-enable no-unused-vars */
 
 // FIXME: Why indent broken when using babel parser?
 /* eslint-disable react/jsx-indent */
-export default class Projects extends React.Component {
+export default class LinkBar extends React.Component {
     render() {
-        return <div className='projects'>[Projects]</div>
+        return <div className='link-bar'>[Links]</div>
     }
 }
 /* eslint-enable react/jsx-indent */
