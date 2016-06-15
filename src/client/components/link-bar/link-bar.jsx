b8a75c81f86bc5f8b9f366da44fa4413f4ffb26f
Jon Hall <jon_hall@outlook.com>
2016-06-15 19:14:38 +0100

    link skeletons

--- src/client/components/link-bar/link-bar.jsx
+++ src/client/components/link-bar/link-bar.jsx
@@ -7,8 +7,27 @@ import __style from './link-bar.styl'
 // FIXME: Why indent broken when using babel parser?
 /* eslint-disable react/jsx-indent */
 export default class LinkBar extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            links: [{
+                url: 'https://github.com/jon-hall',
+                title: 'github/jon-hall',
+                class: 'github-link'
+            }, {
+                url: 'mailto:jon_hall@outlook.com',
+                title: 'email',
+                class: 'email-link'
+            }]
+        }
+    }
+
     render() {
-        return <div className='link-bar'>[Links]</div>
+        const links = this.state.links.map((link) =>
+            <a href={link.url} title={link.title} className={'link ' + link.class}></a>
+        )
+
+        return <div className='link-bar'>{links}</div>
     }
 }
 /* eslint-enable react/jsx-indent */
