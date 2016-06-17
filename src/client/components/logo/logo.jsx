a40dd56b71789274e2812fbd22e8a34eb93bb84a
Jon Hall <jon_hall@outlook.com>
2016-06-17 17:40:37 +0100

    half wave logo

--- src/client/components/logo/logo.jsx
+++ src/client/components/logo/logo.jsx
@@ -10,7 +10,7 @@ export default class App extends React.Component {
     constructor() {
         super()
 
-        const animations = ['twister', 'hover-ring']
+        const animations = ['twister', 'hover-ring', 'half-wave']
         this.state = {
             animation: animations[Math.floor(animations.length * Math.random())]
         }
